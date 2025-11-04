f955ae3fe46c500e1e6dca484e952c20e7517463
SkullSkif <likvlad662@gmail.com>
2025-11-05 00:44:43 +0700

    lab8 begin

---
+++ lab8/md5test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include "MD5.h"
+
+int main() {
+    std::string s = "I love ZI";
+    std::string hash = md5(s); // Получаем hash сообщения
+    std::vector<long long> hash_vt = HashToVector(hash); //Перевод hash в целочисленный vector из 8 элеметов
+    long long * hash_arr = HashToArray(hash); //Перевод hash в целочисленный массив из 8 элеметов
+    // for(auto item : hash_vt){
+    //     std::cout << item << ' ';
+    // }
+    std::cout << hash;
+}
